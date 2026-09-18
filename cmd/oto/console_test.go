e93fb5a202eea82ec4e42c538a8afdb59b449a47
ggmolly <molly@molly.sh>
2026-09-18 02:07:07 +0200

    test(cmd): replace assertion boilerplate with shared helpers

--- cmd/oto/console_test.go
+++ cmd/oto/console_test.go
@@ -8,16 +8,10 @@ import (
 
 func TestConsoleInputErrorsAndEOF(t *testing.T) {
 	var out bytes.Buffer
-	if err := runConsole(strings.NewReader("\"unfinished\nquit\n"), &out, nil, false); err != nil {
-		t.Fatal(err)
-	}
-	if !strings.Contains(out.String(), "unfinished command quote") {
-		t.Fatal(out.String())
-	}
+	must(t, runConsole(strings.NewReader("\"unfinished\nquit\n"), &out, nil, false))
+	failIf(t, !strings.Contains(out.String(), "unfinished command quote"), out.String())
 	if err := runConsole(strings.NewReader(strings.Repeat("x", 129<<10)), &out, nil, false); err == nil {
 		t.Fatal("oversized console command accepted")
 	}
-	if err := runConsole(strings.NewReader("\nquit\n"), &out, nil, true); err != nil {
-		t.Fatal(err)
-	}
+	must(t, runConsole(strings.NewReader("\nquit\n"), &out, nil, true))
 }
